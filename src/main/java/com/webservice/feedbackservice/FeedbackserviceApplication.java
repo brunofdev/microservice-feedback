8fcf4872148de62e13d8538898aae5662e04359e
Bruno de Fraga <brunofragaa97@gmail.com>
2025-09-24 09:28:55 -0300

    removido linha em branco

--- src/main/java/com/webservice/feedbackservice/FeedbackserviceApplication.java
+++ src/main/java/com/webservice/feedbackservice/FeedbackserviceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FeedbackserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FeedbackserviceApplication.class, args);
 	}
-
 }
