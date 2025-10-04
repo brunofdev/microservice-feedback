32dd32913ded999aac99fb5c3884049b63b21af4
Bruno de Fraga <brunofragaa97@gmail.com>
2025-10-04 17:00:03 -0300

    ajustado para as mensagens do rabbit darem ack mesmo com exception para teste

--- src/main/java/com/webservice/feedbackservice/sistema/consumer/FeedbackConsumer.java
+++ src/main/java/com/webservice/feedbackservice/sistema/consumer/FeedbackConsumer.java
@@ -15,8 +15,12 @@ public class FeedbackConsumer {
 
     @RabbitListener(queues = QUEUE_NAME)
     public void receiveMessage(FeedbackDTO feedbackDTO) {
-        System.out.println("Mensagem recebida. Delegando para o serviço de processamento.");
-        feedbackService.saveNewFeedback(feedbackDTO); // Delega o processamento
+       try {
+           System.out.println("Mensagem recebida. Delegando para o serviço de processamento.");
+           feedbackService.saveNewFeedback(feedbackDTO); // Delega o processamento
+       }catch (Exception e){
+           System.out.println ("Erro ao processar mensagem: " + e.getMessage());
+       }
     }
 }
 
